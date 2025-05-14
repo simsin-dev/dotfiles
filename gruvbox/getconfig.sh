6fea3b531d6f3788070c2204dbb7224af1e20af9
simsin-dev <jaculator007@gmail.com>
2025-05-14 18:04:57 +0200

    initial commit

---
+++ gruvbox/getconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp ~/.config/hypr/* ./hypr/
+cp ~/.config/kitty/* ./kitty/
+cp ~/.config/fastfetch/* ./fastfetch/
+cp ~/.config/mako/* ./mako/
+cp ~/.config/nvim/init.lua ./nvim/
+cp ~/.config/waybar/* ./waybar/
+cp ~/wallpapers/* ./images/
